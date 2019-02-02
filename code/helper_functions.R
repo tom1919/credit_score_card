f49d544222ce02cb1dd51704457e69f5aa2c0a41
tom1919 <tommy_lang@users.noreply.github.com>
2019-02-02 13:39:45 -0500

    created graphs

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -197,3 +197,50 @@ allocate_points <- function(mod, df, pdo, score, odds) {
   df$Score <- rowSums(df[, colini:colend])
   return(df)
 }
+
+draw_confusion_matrix <- function(cm) {
+  
+  layout(matrix(c(1,1,2)))
+  par(mar=c(2,2,2,2))
+  plot(c(100, 345), c(300, 450), type = "n", xlab="", ylab="", xaxt='n', yaxt='n')
+  title('CLASSIFICATION TABLE', cex.main=2)
+  
+  # create the matrix 
+  rect(150, 430, 240, 370, col='#3F97D0')
+  text(195, 440, 'No Default', cex=1.4, font=1) # level 0
+  rect(250, 430, 340, 370, col='#F7AD50')
+  text(295, 440, 'Defaulted', cex=1.4, font=1) # level 1
+  text(125, 370, 'Predicted', cex=1.8, srt=90, font = 2)
+  text(245, 450, 'Actual', cex=1.8, font = 2)
+  rect(150, 305, 240, 365, col='#F7AD50')
+  rect(250, 305, 340, 365, col='#3F97D0')
+  text(140, 400, 'No Default', cex=1.4, srt=90, font=1) # level 0
+  text(140, 335, 'Defaulted', cex=1.4, srt=90, font=1) # level 1
+  
+  # add in the cm results 
+  res <- as.numeric(cm$table)
+  text(195, 400, res[1], cex=1.9, font=2, col='white')
+  text(195, 335, res[2], cex=1.9, font=2, col='white')
+  text(295, 400, res[3], cex=1.9, font=2, col='white')
+  text(295, 335, res[4], cex=1.9, font=2, col='white')
+  
+  # add in the specifics 
+  plot(c(100, 0), c(100, 0), type = "n", xlab="", ylab="", main = 
+         "ASSOCIATED STATISTICS", xaxt='n', yaxt='n', cex=1.8, font = 2)
+  text(10, 85, names(cm$byClass[1]), cex=1.2, font=3)
+  text(10, 70, round(as.numeric(cm$byClass[1]), 3), cex=1.2)
+  text(30, 85, names(cm$byClass[2]), cex=1.2, font=3)
+  text(30, 70, round(as.numeric(cm$byClass[2]), 3), cex=1.2)
+  text(50, 87, names(cm$byClass[5]), cex=1.2, font=3)
+  text(50, 71, round(as.numeric(cm$byClass[5]), 3), cex=1.2)
+  text(70, 87, names(cm$byClass[6]), cex=1.2, font=3)
+  text(70, 71, round(as.numeric(cm$byClass[6]), 3), cex=1.2)
+  text(90, 87, names(cm$byClass[7]), cex=1.2, font=3)
+  text(90, 71, round(as.numeric(cm$byClass[7]), 3), cex=1.2)
+  
+  # add in the accuracy information 
+  text(30, 35, names(cm$overall[1]), cex=1.5, font=3)
+  text(30, 19, round(as.numeric(cm$overall[1]), 3), cex=1.4)
+  text(70, 35, names(cm$overall[2]), cex=1.5, font=3)
+  text(70, 19, round(as.numeric(cm$overall[2]), 3), cex=1.4)
+}  
